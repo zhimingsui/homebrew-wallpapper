1768b94872055c0589a6812b857ca37bba4c8171
Zhiming <33706763+zhimingsui@users.noreply.github.com>
2022-01-03 01:22:33 +0800

    Update wallpapper.rb

--- wallpapper.rb
+++ wallpapper.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Wallpapper < Formula
-  homepage 'https://github.com/mczachurski/wallpapper'
-  url 'https://github.com/mczachurski/wallpapper/archive/1.7.2.tar.gz'
+  homepage 'https://github.com/zhimingsui/wallpapper'
+  url 'https://github.com/zhimingsui/wallpapper/archive/1.7.2.tar.gz'
   sha256 '9f8dc5059256731903daa66e510332b6b4a5e3f217ba658c208acbc3d5d97dd6'
 
-  head 'https://github.com/mczachurski/wallpapper.git'
+  head 'https://github.com/zhimingsui/wallpapper.git'
 
   depends_on :xcode => :build
   depends_on :macos => :mojave
